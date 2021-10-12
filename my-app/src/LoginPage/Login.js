abfe7669570e36bbe5757f8dd3ad78ae1e2c68f6
Test pc <testpc@gswvss.com>
2021-10-12 17:23:54 +0530

    login api

--- my-app/src/LoginPage/Login.js
+++ my-app/src/LoginPage/Login.js
@@ -1,16 +1,41 @@
 import React, { useState } from 'react'
 import './Login.css'
 
+const initialValue = { username: "", password: "" }
+
 function Login() {
 
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+    const [value, setValue] = useState(initialValue);
+
+    const login = () => {
+
+        const data = {
+            "email": value.username,
+            "password": value.password,
+        }
+
+        const options = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        }
+
+        const fetchRes = fetch("https://reqres.in/api/login", options)
+        return fetchRes;
+    }
 
     const handleSubmit = (e) => {
+        const response = login();
         e.preventDefault();
-        alert('Username is: ' + username);
-        setUsername('');
-        setPassword('');
+        response.then(res => res.json()).then(data => {
+            if (data.token) {
+                alert("Login sucessful");
+                setValue(initialValue);
+            }
+            else {
+                alert(data.error);
+            }
+        })
     }
 
     return (
@@ -19,8 +44,8 @@ function Login() {
                 <h3>LOGIN</h3>
                 <p>Please enter your credentials to LOGIN</p>
                 <form className="login-form" onSubmit={handleSubmit}>
-                    <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
-                    <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <input type="text" placeholder="Username" value={value.username} onChange={(e) => setValue({ ...value, username: e.target.value })} />
+                    <input type="password" placeholder="Password" value={value.password} onChange={(e) => setValue({ ...value, password: e.target.value })} />
                     <button type="submit">Login</button>
                 </form>
             </div>
